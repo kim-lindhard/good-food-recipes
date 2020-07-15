29cc938182e47b60ebd174caaaaeb9595cded530
kimlindhard <kilin@dfds.com>
2020-07-15 14:07:44 +0200

    add initial solution structure

---
+++ test/GoodFood.AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GoodFood.AcceptanceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
